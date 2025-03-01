004f6359c9d9a786daa6ba967a47657190d95ec1
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-03-01 17:27:11 +0100

    Estructura proyecto Maven

---
+++ java/Futbolistos/src/main/java/PDS/Futbolistos/App.java
@@ -0,0 +1,13 @@
+package PDS.Futbolistos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
